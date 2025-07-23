7eab608ce9c5d3316948e7df0ab69ffc29fc0c7e
c1t2n6 <nguyen12690@gmail.com>
2025-07-23 16:34:00 +0700

    Chuyển sang SQLite cho Render

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/library"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
