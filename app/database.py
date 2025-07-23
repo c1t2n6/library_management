f6ea7a7d1f45a76ec9212212059ee4de09d8bb46
c1t2n6 <nguyen12690@gmail.com>
2025-07-23 16:40:35 +0700

    try

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
+DATABASE_URL = "sqlite:///./library.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
